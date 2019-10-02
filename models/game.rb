288f26f9a38fb9336118752c1ef0c5798ea9b0e1
Laurie Frank <laurie360@hotmail.co.uk>
2019-10-02 07:43:20 +0100

    mvp complete

--- models/game.rb
+++ models/game.rb
@@ -1,12 +1,12 @@
 class Game
 
   def self.play_game(hand1, hand2)
-    if (hand1 = "Rock" && hand2 = "Scissors") || (hand2 = "Rock" && hand1 = "Scissors")
-      puts "Rock wins"
-    elsif (hand1 = "Paper" && hand2 = "Rock") || (hand2 = "Paper" && hand1 = "Rock")
-      puts "Paper wins"
-    else (hand1 = "Scissors" && hand2 = "Paper") || (hand2 = "Scissors" && hand1 = "Paper")
-      puts "Scissors wins"
+    if (hand1 == "rock" && hand2 == "scissors") || (hand2 == "rock" && hand1 == "scissors")
+      return "Rock wins"
+    elsif (hand1 == "paper" && hand2 == "rock") || (hand2 == "paper" && hand1 == "rock")
+      return "Paper wins"
+    elsif (hand1 == "scissors" && hand2 == "paper") || (hand2 == "scissors" && hand1 == "paper")
+      return "Scissors wins"
     end
   end
 
